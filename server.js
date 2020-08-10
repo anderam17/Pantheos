12daa56f02d4ad029ed9424ab2d1e1d347ab62b5
Allana Anderson <allana.anderson17@gmail.com>
2020-08-10 13:56:13 -0600

    changed config.json for JAWS

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const PORT = process.env.PORT || 8080;
 const db = require("./models");
 
 app.use(express.urlencoded({
-    extended: true
+  extended: true
 }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
 app.engine("handlebars", exphbs({
-    defaultLayout: "main"
+  defaultLayout: "main"
 }));
 
 app.set("view engine", "handlebars");
@@ -25,7 +25,7 @@ require("./routes/teacher-api-routes")(app);
 require("./routes/student-api-routes")(app);
 
 db.sequelize.sync().then(function () {
-    app.listen(PORT, function () {
-        console.log("App listening on PORT " + PORT);
-    });
+  app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+  });
 });
