4008b3b7ac4d00c51306bc132afe221ed6baff98
Allana Anderson <allana.anderson17@gmail.com>
2020-08-10 10:36:46 -0600

    made a few minor changes to wording in student cards. Also added spacing between the cards.

--- public/JS/script.js
+++ public/JS/script.js
@@ -79,14 +79,14 @@ $(document).ready(function () {
 
   const renderStudentCard = (teacher, student) => {
     $("#studentCard").append(
-      `<div data-id= "${student.id}" class="card">
+      `<div data-id= "${student.id}" class="card" style="margin-top: 20px">
         <div class="card-header">
         Name: ${student.first_name} ${student.last_name}
         </div>
         <div class="card-body">
       <p class="card-text studentGrade">Grade: ${student.grade}</p>
-      <p class="card-text teacher">Teacher: ${teacher.first_name} ${teacher.last_name}</p>
-      <p class="card-text studentDetention">In Detention?: ${student.detention}</p> 
+      <p class="card-text teacher">Homeroom Teacher: ${teacher.first_name} ${teacher.last_name}</p>
+      <p class="card-text studentDetention">Has Detention?: ${student.detention}</p> 
 
       <a href="/update.html" class="btn btn-primary" data-id=${student.id} id="edit">Edit</a>
       <a class="btn btn-warning" data-id=${student.id}>Detention</a>
