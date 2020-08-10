9679730b3a4157aca9c105863989af88979509ce
Kimberly Owens <kimberly.j.owens@gmail.com>
2020-08-10 11:06:06 -0600

    adding code to change class

--- public/JS/script.js
+++ public/JS/script.js
@@ -3,6 +3,18 @@ $(document).ready(function () {
     window.location.href = "/student";
   })
 
+  const gradeClass = (student) => {
+    let studentGrade = student.grade;
+
+    if (studentGrade === 6) {
+      $("#cardBody").addClass("sixth")
+    } else if (studentGrade === 7) {
+      $("#cardBody").addClass("seventh")
+    } else {
+      $("#cardBody").addClass("eighth")
+    }
+  }
+
 
 
   // ------- SEARCH BY TEACHER ----------- 
@@ -21,6 +33,7 @@ $(document).ready(function () {
           let teacher = data[0];
           console.log(student);
           renderStudentCard(teacher, student);
+          gradeClass(student);
         }
       } else {
         $("#studentCard").append(`<h4>This teacher has no students assigned to them.</h4>`)
@@ -43,6 +56,7 @@ $(document).ready(function () {
         const teacher = data[i].Teacher;
         const student = data[i];
         renderStudentCard(teacher, student);
+       
       }
     });
 
@@ -69,22 +83,24 @@ $(document).ready(function () {
 
   // ------- FUNCTION TO RENDER STUDENT CARDS ----------- 
   const renderStudentCard = (teacher, student) => {
+
     $("#studentCard").append(
       `<div data-id= "${student.id}" class="card">
         <div class="card-header">
         <h5>Student: ${student.first_name} ${student.last_name}</h5>
         </div>
-        <div class="card-body">
-      <p class="card-text studentGrade">Grade: ${student.grade}</p>
+        <div class="card-body" id="cardBody">
+      <p class="card-text studentGrade" data-grade=${student.grade}>Grade: ${student.grade}</p>
       <p class="card-text teacher">Teacher: ${teacher.first_name} ${teacher.last_name}</p>
       <p class="card-text studentDetention"> Detention: ${student.detention? "Yes" : "No"}</p> 
 
       <a class="btn btn-primary" data-id=${student.id} id="edit">Edit</a>
-      <a class="btn btn-warning" data-id=${student.id}>Detention</a>
+      <a class="btn btn-warning" data-id=${student.id} data-detention=${student.detention} id="detentionBtn" >Detention</a>
       <a class="btn btn-danger" id = "deleteBtn" data-id=${student.id}>Delete</a>
       
       </div>
       </div>`);
+      
   };
 
   // ------- DELETE STUDENT ----------- 
@@ -109,4 +125,32 @@ $(document).ready(function () {
     window.location.href = "/editstudent?student_id=" + studentId;
   });
 
+  // ------- HAS DETENTION ----------- 
+  $("#studentCard").on("click", "#detentionBtn", function (event) {
+    event.preventDefault();
+
+    let studentId = $(this).data("id");
+    let hasDetention = $(this).data("detention");
+    console.log(hasDetention);
+
+   if (!hasDetention) {
+     hasDetention = true
+   } else {
+     hasDetention = false
+   };
+   console.log(hasDetention);
+
+   let hasDetentionState = {
+     detention: hasDetention
+   };
+
+    $.ajax("api/student/" + studentId, {
+      type: "PUT",
+      data: hasDetentionState
+    }).then(answer => {
+      // $(`[data-id=${studentId}]`).update();
+    });
+  });
+
+ 
 });
